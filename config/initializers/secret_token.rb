5d589a4c7ac82276e94b316415b48d92f60f8269
Nick <nrwineinger@gmail.com>
2016-01-31 13:58:49 -0700

    Initial Commit with Rails New

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BandStache::Application.config.secret_key_base = '1bb4c4a42b00cd074090c08365426b1814fc7466a2b95a7f4d8b99e38085c8894a1301d5f4d7a2db6b7792cf9538efee2be8fc9f3c49b3eb2bf0a38c37682b7b'
